9b0f0fe624d34f97a3cb4c9f4d42aafd52c297f3
Maya Shen <hs45@rice.edu>
2022-03-28 15:11:59 -0400

    restructured

---
+++ to-gather-frontend/src/pages/Landing.jsx
@@ -0,0 +1,15 @@
+import { Link } from "react-router-dom";
+import React from "react";
+import Navbar from "../components/Navbar";
+
+function Landing() {
+  return (
+    <>
+      <Navbar />
+
+      <h1>Landing Page</h1>
+    </>
+  );
+}
+
+export default Landing;
