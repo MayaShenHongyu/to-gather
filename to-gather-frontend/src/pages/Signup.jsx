3694a71baef9fb2e8efb6045e8066dcb48b9fb5f
Maya Shen <hs45@rice.edu>
2022-03-28 21:19:09 -0400

    Remove old sign in page

--- to-gather-frontend/src/pages/Signup.jsx
+++ to-gather-frontend/src/pages/Signup.jsx
@@ -1,4 +1,4 @@
-import { React, useState } from "react";
+import { React } from "react";
 import { Navigate, useLocation } from "react-router-dom";
 import { useAuth } from "../contexts/AuthContext";
 import {
