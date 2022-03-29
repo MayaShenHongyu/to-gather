7501214f717f012f18751fcdac9d2a3971ba8b6d
xuanyufang <42874337+xuanyufang@users.noreply.github.com>
2022-03-29 17:28:30 -0400

    dashboard + background + logo link

--- to-gather-frontend/src/pages/Dashboard.js
+++ to-gather-frontend/src/pages/Dashboard.js
@@ -1,13 +1,28 @@
 import React from "react";
 import { useAuth } from "../contexts/AuthContext";
 import Navbar from "../components/Navbar";
+import BannerImage from "../assets/friends.png";
+import Footer from "../components/Footer";
 
 export default function Dashboard() {
   const { currentUser } = useAuth();
   return (
-    <div>
-      <Navbar />
-      {currentUser.email}
+    <>
+    <Navbar />
+    <div
+      className="landing"
+      style={{ backgroundImage: `url(${BannerImage})` }}
+    >
+      <div className="headerContainer">
+        <h1
+          style={{ fontWeight: "bold", textAlign: "center", color: "white" }}
+        >
+          {" "}
+          To-Gather Dashboard{" "}
+        </h1>
+      </div>
     </div>
+    <Footer />
+  </>
   );
 }
