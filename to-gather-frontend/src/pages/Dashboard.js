0aebb3976b175af3965463fcab5c9d6ce952bab0
Maya Shen <hs45@rice.edu>
2022-04-23 00:21:50 -0400

    dashboard

--- to-gather-frontend/src/pages/Dashboard.js
+++ to-gather-frontend/src/pages/Dashboard.js
@@ -1,32 +1,49 @@
-import React from "react";
+import React, { useState } from "react";
 import { useAuth } from "../contexts/AuthContext";
 import Navbar from "../components/Navbar";
+import MultiSelect from "../components/MultiSelect";
+import EventCard from "../components/EventCard";
 import BannerImage from "../assets/friends.png";
-import Footer from "../components/Footer";
+// import Footer from "../components/Footer";
 import { createEvent } from "../backend";
+import "./Dashboard.css";
+
+const categories = ["Sports", "Kids", "Music", "Board Games"];
 
 export default function Dashboard() {
   const { currentUser } = useAuth();
-  const newEvent = () =>
-    createEvent(currentUser.uid, { name: "event name", time: Date.now() });
+  const [selectedCategories, setSelectedCategories] = useState([]);
+
   return (
-    <>
-      <Navbar />
-      <div
-        className="landing"
-        style={{ backgroundImage: `url(${BannerImage})` }}
-      >
-        <div className="headerContainer">
-          <button onClick={newEvent}>New event</button>
-          <h1
-            style={{ fontWeight: "bold", textAlign: "center", color: "white" }}
-          >
-            {" "}
-            To-Gather Dashboard{" "}
-          </h1>
+    <div className="landing" style={{ backgroundImage: `url(${BannerImage})` }}>
+      <div className="page-frame">
+        <Navbar />
+        <div className="content">
+          <div className="header">
+            <div className="title">Events near you</div>
+            <div className="search-bar">
+              <MultiSelect
+                label="Filter category"
+                options={categories}
+                selected={selectedCategories}
+                setSelected={setSelectedCategories}
+              />
+            </div>
+          </div>
+          <div className="events-wrapper">
+            <EventCard
+              title="Wonder Girls 2010 Wonder Girls World Tour San Francisco"
+              description="We’ll get you directly seated and inside for you to enjoy the show."
+              date={new Date(2021, 3, 20)}
+            />
+            <EventCard />
+            <EventCard />
+            <EventCard />
+            <EventCard />
+            <EventCard />
+          </div>
         </div>
       </div>
-      <Footer />
-    </>
+    </div>
   );
 }
