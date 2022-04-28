4a6ced96b6b24f459193cdc19fd91793e1692ab0
Maya Shen <hs45@rice.edu>
2022-04-27 22:04:33 -0400

    added modal for event details

--- to-gather-frontend/src/pages/Dashboard.js
+++ to-gather-frontend/src/pages/Dashboard.js
@@ -6,7 +6,13 @@ import EventCard from "../components/EventCard";
 import BannerImage from "../assets/background.jpg";
 // import Footer from "../components/Footer";
 import { getFilteredEvents, upLoadImage } from "../backend";
-import { FormControl, Select, MenuItem, InputLabel } from "@mui/material";
+import {
+  FormControl,
+  Select,
+  MenuItem,
+  InputLabel,
+  Modal,
+} from "@mui/material";
 
 import "./Dashboard.css";
 
@@ -17,6 +23,7 @@ export default function Dashboard() {
   const [categoryFilter, setCategoryFilter] = useState([]);
   const [dateFilter, setDateFilter] = useState("anytime");
   const [events, setEvents] = useState([]);
+  const [selectedEvent, setSelectedEvent] = useState();
 
   useEffect(() => {
     const now = new Date();
@@ -36,6 +43,23 @@ export default function Dashboard() {
     );
   }, [dateFilter, categoryFilter]);
 
+  const dateFilterSelector = (
+    <FormControl size="small" sx={{ m: 1, width: 200 }}>
+      <InputLabel id="date-filter">Date</InputLabel>
+      <Select
+        labelId="date-filter"
+        value={dateFilter}
+        label="Date"
+        onChange={(event) => setDateFilter(event.target.value)}
+      >
+        <MenuItem value={"anytime"}>Anytime</MenuItem>
+        <MenuItem value={"today"}>Today</MenuItem>
+        <MenuItem value={"in three days"}>In three days</MenuItem>
+        <MenuItem value={"in a week"}>In a week</MenuItem>
+      </Select>
+    </FormControl>
+  );
+
   return (
     <div className="landing" style={{ backgroundImage: `url(${BannerImage})` }}>
       <div className="page-frame">
@@ -44,20 +68,7 @@ export default function Dashboard() {
           <div className="header">
             <div className="title">Events near you</div>
             <div className="search-bar">
-              <FormControl size="small" sx={{ m: 1, width: 200 }}>
-                <InputLabel id="date-filter">Date</InputLabel>
-                <Select
-                  labelId="date-filter"
-                  value={dateFilter}
-                  label="Date"
-                  onChange={(event) => setDateFilter(event.target.value)}
-                >
-                  <MenuItem value={"anytime"}>Anytime</MenuItem>
-                  <MenuItem value={"today"}>Today</MenuItem>
-                  <MenuItem value={"in three days"}>In three days</MenuItem>
-                  <MenuItem value={"in a week"}>In a week</MenuItem>
-                </Select>
-              </FormControl>
+              {dateFilterSelector}
               <MultiSelect
                 label="Filter category"
                 options={categories}
@@ -69,6 +80,7 @@ export default function Dashboard() {
           <div className="events-wrapper">
             {events.map((e, idx) => (
               <EventCard
+                onClick={() => setSelectedEvent(idx)}
                 key={idx}
                 title={e.name}
                 date={e.time}
@@ -76,8 +88,21 @@ export default function Dashboard() {
                 description={e.description}
               />
             ))}
+            <EventCard />
+            <EventCard />
+            <EventCard />
+            <EventCard />
+            <EventCard />
+            <EventCard />
+            <EventCard />
           </div>
         </div>
+        <Modal
+          open={selectedEvent != undefined}
+          onClose={() => setSelectedEvent(undefined)}
+        >
+          <div className="event-modal">!! Event page goes here !!</div>
+        </Modal>
       </div>
     </div>
   );
